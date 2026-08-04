b78dfa9b9f92aff44289b9c4ead62bf89d10b10d
AndreyVjs <andreyvjs@gmail.com>
2026-08-04 11:52:10 -0300

    Refactor Home component and add navigation

--- navStack/Home.js
+++ navStack/Home.js
@@ -1,5 +1,14 @@
-import{View,Text}from 'react-native'
+import{View,Text}from 'react-native';
+import {useNavigation}from '@react-navigation/native';
+export default function Home(){
 
-export default function home(){
-    return (<view><text>Home</text></view>)
+    const nav = useNavigation();
+    return(
+    <View>
+        <Text>Bem vindo!!</Text>
+        <Button onPress={() => nav.navigate('Sobre')}></Button>
+    </View>)
 }
+
+//Criar o projeto
+//npx create-expo-app@latest form  --template blank
